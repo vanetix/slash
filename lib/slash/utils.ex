ad3877a1de5e1e865e17c21d322e9fc0deb798e3
Matt McFarland <783024+vanetix@users.noreply.github.com>
2019-03-25 13:18:59 -0500

    Fix dialyzer warning about invalid typespec

--- lib/slash/utils.ex
+++ lib/slash/utils.ex
@@ -18,7 +18,7 @@ defmodule Slash.Utils do
   @doc """
   Handle a response from a command block.
   """
-  @spec build_response_payload(Slash.Builder.command_response()) :: String.t()
+  @spec build_response_payload(Slash.Builder.command_response()) :: map()
   def build_response_payload(response) when is_binary(response), do: %{text: response}
   def build_response_payload(response) when is_map(response), do: response
   def build_response_payload(:async), do: %{}
