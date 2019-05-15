c78810a3db77e37a917c2b957fcab6b48a42bdf9
Matt McFarland <783024+vanetix@users.noreply.github.com>
2019-05-15 14:06:48 -0500

    Fix issue caused by sending empty text response to slack

--- lib/slash/utils.ex
+++ lib/slash/utils.ex
@@ -9,11 +9,16 @@ defmodule Slash.Utils do
   Sends a json response to the `conn`.
   """
   @spec send_json(Conn.t(), map(), number()) :: Conn.t()
-  def send_json(conn, response, status \\ 200) do
+  def send_json(conn, response, status \\ 200)
+
+  def send_json(conn, response, status) when is_binary(response) do
     conn
     |> Conn.put_resp_content_type("application/json")
-    |> Conn.send_resp(status, Jason.encode!(response))
+    |> Conn.send_resp(status, response)
   end
+
+  def send_json(conn, response, status),
+    do: send_json(conn, Jason.encode!(response), status)
 
   @doc """
   Handle a response from a command block.
@@ -21,7 +26,7 @@ defmodule Slash.Utils do
   @spec build_response_payload(Slash.Builder.command_response()) :: map()
   def build_response_payload(response) when is_binary(response), do: %{text: response}
   def build_response_payload(response) when is_map(response), do: response
-  def build_response_payload(:async), do: %{text: ""}
+  def build_response_payload(:async), do: ""
 
   def build_response_payload(response) do
     raise ArgumentError, """
